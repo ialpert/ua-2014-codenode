a029fd379d816faaa1df0c9c1e581e4af21c2909
Andrii Gordiichuk <andriigordiichuk@Andriis-MacBook-Pro.local>
2014-11-29 15:45:20 +0200

    [DRAFT] Add base structure for whiteBoard directive

--- client/src/components/whiteBoard/whiteBoard.directive.js
+++ client/src/components/whiteBoard/whiteBoard.directive.js
@@ -1,1 +1,22 @@
+'use strict';
 
+angular.module('interviewer')
+  .directive('whiteBoard', function (AppState) {
+
+    var paper, project, view;
+
+    paper = window.paper;
+    project = window.project;
+    view = window.view;
+
+    return {
+      restrict:'AEC',
+      templateUrl: 'components/whiteBoard/whiteBoard.html',
+      controller: function($scope) {
+
+      },
+      link: function(scope, element, attrs) {
+
+      }
+    };
+  });
