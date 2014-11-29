095471ce68e880235f398c285850be028a9a3264
Igor Alpert <igor_alpert@epam.com>
2014-11-29 14:50:28 +0200

    [DRAFT] Interview routing setup+join

--- server/routes/interview.js
+++ server/routes/interview.js
@@ -1,14 +1,78 @@
 'use strict';
 
 module.exports = function(store) {
+
+  var model = store.createModel();
+
   return {
 
     setup: function(data, fn) {
+      var userRef, sessionRef, head, session;
 
+      userRef = model.id();
+      sessionRef = model.id();
+
+      head = {
+        name: data.data.name,
+        status: 'head',
+        interview: sessionRef
+      };
+
+      session = {};
+
+      model.set('users.' + userRef, head, function() {
+        model.set('interviewSession.' + sessionRef, session, function() {
+
+          fn({
+            result: {
+              accessId: userRef
+            },
+
+            error: null
+          });
+
+        });
+      });
     },
 
     join: function(data, fn) {
+      var user, sessionRef, userRef, people;
 
+      userRef = 'users.' + data.token;
+
+      model.fetch(userRef, function() {
+        user = model.get(userRef);
+
+        if (user && user.interview) {
+
+          people = model.query('people', {interview: user.interview});
+          people.subscribe();
+
+          model.set('_page.token', data.token);
+          model.set('_page.sessionToken', user.interview);
+
+          sessionRef = 'interviewSession.' + user.interview;
+
+          model.ref('_page.user', userRef);
+          model.ref('_page.session', sessionRef);
+
+          model.subscribe('_page.user', '_page.session', function() {
+
+            model.bundle(function(err, bundle) {
+
+              fn({
+                result: bundle,
+                error: null
+              });
+            });
+          });
+
+        } else {
+          fn({
+            error: 'Invalid user session token'
+          });
+        }
+      });
     }
 
   };
--- server/test/interviewSpec.js
+++ server/test/interviewSpec.js
@@ -4,10 +4,16 @@ require('../');
 
 require('./socketSuite')({describe: 'Interview IO', socketURL: 'http://localhost:3100'}, function(suite) {
 
+  var headAccessId;
+
   it('interview:setup', function(done) {
 
-    this.socket.emit('interview:setup', {name: 'Test head'}, function(data) {
+    this.socket.emit('interview:setup', {data: {name: 'Test head'}}, function(data) {
 
+      suite.success(data);
+      expect(data.result.accessId).toBeDefined();
+
+      headAccessId = data.result.accessId;
       done();
     });
 
@@ -15,7 +21,15 @@ require('./socketSuite')({describe: 'Interview IO', socketURL: 'http://localhost
 
   it('interview:join', function(done) {
 
-    this.socket.emit('interview:join', {}, function(data) {
+    this.socket.emit('interview:join', {token: headAccessId}, function(data) {
+      var page;
+
+      suite.success(data);
+      page = data.result.collections._page;
+
+      expect(page.token).toBeDefined();
+      expect(page.sessionToken).toBeDefined();
+      expect(page.user).toBeDefined();
 
       done();
     });
