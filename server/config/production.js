a3715da6c0e323740eadd881d4666337eaff830d
Igor Alpert <igor_alpert@epam.com>
2014-11-30 01:48:44 +0200

    [DRAFT] MongoDb / Deploys

--- server/config/production.js
+++ server/config/production.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   mongoose: {
-    connection: 'mongodb://localhost/interviewer-dev',
+    connection: 'mongodb://nodejitsu:408cf1f78d7df5d2edc2edac78553178@troup.mongohq.com:10073/nodejitsudb2934462456',
     options: {
       server: {
         auto_reconnect: true
